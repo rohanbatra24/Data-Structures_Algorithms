183ef4eaf03d13d8f4bdd4d305c8744e30d7827d
Rohan Batra <rohanbatra24@gmail.com>
2020-08-08 21:31:17 -0400

    different inputs

---
+++ differentInputs.js
@@ -0,0 +1,12 @@
+const compressBoxesTwice = (boxes1, boxes2) => {
+	// two different inputs
+	boxes1.forEach((element) => {
+		console.log(element);
+	});
+
+	boxes2.forEach((element) => {
+		console.log(element);
+	});
+};
+
+// O(a + b) - where boxes1.length = a & boxes2.length = b
