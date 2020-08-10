762413893d83e602d4cfa7383faa3e3acb2471b3
Rohan Batra <rohanbatra24@gmail.com>
2020-08-10 13:30:34 -0400

    first recurring char

---
+++ firstRecurringChar.js
@@ -0,0 +1,31 @@
+//Google Question
+//Given an array = [2,5,1,2,3,5,1,2,4]:
+//It should return 2
+
+//Given an array = [2,1,1,2,3,5,1,2,4]:
+//It should return 1
+
+//Given an array = [2,3,4,5]:
+//It should return undefined
+
+function firstRecurringCharacter(input) {
+	const arr = [];
+
+	for (let num of input) {
+		if (arr.includes(num)) {
+			return num;
+		}
+		arr.push(num);
+	}
+}
+
+function firstRecurringCharacter(input) {
+	const obj = {};
+
+	for (let num of input) {
+		if (obj[num]) {
+			return num;
+		}
+		obj[num] = true;
+	}
+}
