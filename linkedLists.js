ba10095f3b44ccb34d69cacdd722bacd34a4a760
Rohan Batra <rohanbatra24@gmail.com>
2020-08-10 20:04:28 -0400

    add reverse method

--- linkedLists.js
+++ linkedLists.js
@@ -95,6 +95,23 @@ class LinkedList {
 		}
 		return currentNode;
 	}
+
+	reverse() {
+		if (!this.head.next) {
+			return this.head;
+		}
+		let first = this.head;
+		this.tail = this.head;
+		let second = first.next;
+		while (second) {
+			const temp = second.next;
+			second.next = first;
+			first = second;
+			second = temp;
+		}
+		this.head.next = null;
+		this.head = first;
+	}
 }
 
 const MyLinkedList = new LinkedList(10);
