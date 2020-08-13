623142455021e4fcdac26be7ac2c626660e26d19
Rohan Batra <rohanbatra24@gmail.com>
2020-08-13 11:33:13 -0400

    factorial with recursion

--- recursion-factorial.js
+++ recursion-factorial.js
@@ -1,6 +1,7 @@
 // Write two functions that finds the factorial of any number. One should use recursive, the other should just use a for loop
 
 function findFactorialRecursive(number) {
+	// time complexity O(n)
 	//code here
 	if (number === 2) {
 		return 2;
@@ -9,6 +10,7 @@ function findFactorialRecursive(number) {
 }
 
 function findFactorialIterative(number) {
+	// time complexity O(n)
 	//code here
 	let multiplier = 1;
 	for (let i = number; i > 0; i = i - 1) {
