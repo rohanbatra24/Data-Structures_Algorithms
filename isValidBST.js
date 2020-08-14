d37283dba31da60c07ed4f19d09ba5adfb7be804
Rohan Batra <rohanbatra24@gmail.com>
2020-08-14 11:25:16 -0400

    check if valid BST

---
+++ isValidBST.js
@@ -0,0 +1,25 @@
+// check if it is a valid BST
+
+function isValidBST(root, min, max) {
+	if (!root) {
+		return true;
+	}
+
+	if (min !== undefined && root.val <= min) {
+		return false;
+	}
+
+	if (max !== undefined && root.val >= max) {
+		return false;
+	}
+
+	if (root.left && root.left.val >= root.val) {
+		return false;
+	}
+
+	if (root.right && root.right.val <= root.val) {
+		return false;
+	}
+
+	return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+}
