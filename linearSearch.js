ac7e5380ed4a331282924f37357a799ae6d387d2
Rohan Batra <rohanbatra24@gmail.com>
2020-08-14 09:32:45 -0400

    linear search

---
+++ linearSearch.js
@@ -0,0 +1,15 @@
+// all below are linear search
+
+var beasts = [ 'Centaur', 'Godzilla', 'Mosura', 'Minotaur', 'Hydra', 'Nessie' ];
+
+beasts.indexOf('Godzilla');
+
+beasts.findIndex(function(item) {
+	return item === 'Godzilla';
+});
+
+beasts.find(function(item) {
+	return item === 'Godzilla';
+});
+
+beasts.includes('Godzilla');
