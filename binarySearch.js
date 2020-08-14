3a49d4dcadc91ecb28838c6eb05e103808dc8499
Rohan Batra <rohanbatra24@gmail.com>
2020-08-14 09:47:59 -0400

    binary search - recursive and iterative

---
+++ binarySearch.js
@@ -0,0 +1,48 @@
+let arr = [ 1, 3, 5, 7, 8, 9 ];
+let x = 5; // search for x
+
+// Recursive approach - O(logN)
+const recursiveFunction = (arr, x, start, end) => {
+	// Base Condition
+	if (start > end) return false;
+
+	// Find the middle index
+	let mid = Math.floor((start + end) / 2);
+
+	// Compare mid with given key x
+	if (arr[mid] === x) return true;
+
+	// If element at mid is greater than x,
+	// search in the left half of mid
+	if (arr[mid] > x) return recursiveFunction(arr, x, start, mid - 1);
+	else
+		// If element at mid is smaller than x,
+		// search in the right half of mid
+		return recursiveFunction(arr, x, mid + 1, end);
+};
+
+console.log(recursiveFunction(arr, x, 0, arr.length - 1));
+
+// Iterative approach
+
+let iterativeFunction = function(arr, x) {
+	let start = 0;
+	let end = arr.length - 1;
+
+	// Iterate while start doesn't meet end
+	while (start <= end) {
+		// Find the mid index
+		let mid = Math.floor((start + end) / 2);
+
+		// If element is present at mid, return True
+		if (arr[mid] === x) return true;
+		else if (arr[mid] < x)
+			// Else look in left or right half accordingly
+			start = mid + 1;
+		else end = mid - 1;
+	}
+
+	return false;
+};
+
+console.log(iterativeFunction(arr, x));
