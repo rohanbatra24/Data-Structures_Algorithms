58feef065d75d1fd5173ed7340c166bd11457701
Rohan Batra <rohanbatra24@gmail.com>
2020-08-18 10:21:25 -0400

    overlapping intervals

---
+++ overlappingIntervals.js
@@ -0,0 +1,40 @@
+// Given a set of time intervals in any order, merge all overlapping intervals into one and output the result which should have only mutually exclusive intervals. Let the intervals be represented as pairs of integers for simplicity.
+
+// For example, let the given set of intervals be {{1,3}, {2,4}, {5,7}, {6,8} }. The intervals {1,3} and {2,4} overlap with each other, so they should be merged and become {1, 4}. Similarly {5, 7} and {6, 8} should be merged and become {5, 8}
+
+const arr = [ [ 1, 3 ], [ 2, 4 ], [ 5, 7 ], [ 6, 8 ] ];
+
+const mergeIntervals = (intervals) => {
+	// The brute force solution is to do a nested for loop (O(n^2))
+	// and check all intervals with each other, detecting if there are any
+	// overlaps
+
+	// The more efficient solution is to sort the results first, and then
+	// compare intervals added to a stack
+
+	if (intervals.length === 0) return intervals;
+
+	const stack = [];
+	// sort the input based on start time
+	const sorted = intervals.sort((a, b) => a[0] - b[0]);
+
+	// push the first interval to the stack
+	stack.push(sorted[0]);
+
+	for (let i = 1; i < intervals.length; i++) {
+		const topInterval = stack[stack.length - 1];
+
+		// if it's not overlapping, push to stack
+		if (intervals[i][0] > topInterval[1]) {
+			stack.push(intervals[i]);
+		}
+		else {
+			// update last intervals end to the farther ending
+			topInterval[1] = Math.max(intervals[i][1], topInterval[1]);
+		}
+	}
+
+	return stack;
+};
+
+console.log(mergeIntervals(arr));
